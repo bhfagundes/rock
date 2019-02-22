5d5732b2df49bdd2516940bd33a524236f1bf861
Brenner Fagundes <bhfagundes@gmail.com>
2019-02-22 08:31:49 -0300

    Adicicionando arquivos básicos de front-end Vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <mdb-container>
+    <mdb-row class="mt-5 align-items-center justify-content-start">
+      <h4 class="demo-title"><strong>Setup</strong></h4>
+    </mdb-row>
+    <mdb-container class="mt-5 align-items-center justify-content-start">
+
+      <mdb-tabs
+        :active="0"
+        default
+        :links="[
+          { text: 'Home' },
+          { text: 'Profile' },
+          { text: 'Contact' }]"
+        :content="[
+          'sdads',
+          `Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.`,
+          `Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.`]"
+      />
+    </mdb-container>
+  </mdb-container>
+</template>
+
+<script>
+import { mdbContainer, mdbCol, mdbRow, mdbIcon, mdbTabs } from 'mdbvue';
+
+export default {
+  name: 'TabPage',
+  components: {
+    mdbContainer,
+    mdbCol,
+    mdbRow,
+    mdbIcon,
+    mdbTabs
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h4 {
+  font-weight: bold;
+}
+</style>
